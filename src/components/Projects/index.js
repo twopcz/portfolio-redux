02922560611efa3eae234cb75de34cf098b839d5
Justin <twoplatinumchainz@gmail.com>
2020-04-19 13:40:45 -0700

    Mapping projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,60 +1,66 @@
 import React from 'react';
 import './style.css';
 
-const projectsOne = [{}];
-const projectsTwo = [{}];
+const projectsOne = [
+  {
+    title: 'Test',
+    link: 'https://agile-headland-04774.herokuapp.com/',
+    image: 'https://via.placeholder.com/800x1000',
+    alt: `project-1`,
+  },
+  {
+    title: 'Test',
+    link: '',
+    image: 'https://via.placeholder.com/800x1000',
+    alt: `project-2`,
+  },
+  {
+    title: 'Test',
+    link: '',
+    image: 'https://via.placeholder.com/800x1000',
+    alt: `project-3`,
+  },
+];
+
+const projectsTwo = [
+  {
+    link: '',
+    image: 'https://via.placeholder.com/800x1000',
+    alt: `project-4`,
+  },
+  {
+    link: '',
+    image: 'https://via.placeholder.com/800x1000',
+    alt: `project-5`,
+  },
+  {
+    link: '',
+    image: 'https://via.placeholder.com/800x1000',
+    alt: `project-6`,
+  },
+];
 
 function Projects() {
   return (
     <div>
       <div className='row justify-content-center' id='projects-top'>
         <div className='container d-flex flex-row project-container'>
-          <div className='col-md-4'>
-            <img
-              className='img-fluid'
-              src='https://via.placeholder.com/800x1000'
-            ></img>
-            <div className='project-titles text-center'>Test</div>
-          </div>
-          <div className='col-md-4'>
-            <img
-              className='img-fluid'
-              src='https://via.placeholder.com/800x1000'
-            ></img>
-            <div className='project-titles text-center'>Test</div>
-          </div>
-          <div className='col-md-4'>
-            <img
-              className='img-fluid'
-              src='https://via.placeholder.com/800x1000'
-            ></img>
-            <div className='project-titles text-center'>Test</div>
-          </div>
+          {projectsOne.map((data, index) => (
+            <div className='col-md-4' key={`project-column-${index}`}>
+              <img className='img-fluid' src={data.image} alt={data.alt}></img>
+              <div className='project-titles text-center'>{data.title}</div>
+            </div>
+          ))}
         </div>
       </div>
       <div className='row justify-content-center py-4' id='projects-bottom'>
         <div className='container d-flex flex-row project-container'>
-          <div className='col-md-4'>
-            <img
-              className='img-fluid'
-              src='https://via.placeholder.com/800x1000'
-            ></img>
-            <div className='project-titles text-center'>Test</div>
-          </div>
-          <div className='col-md-4'>
-            <img
-              className='img-fluid'
-              src='https://via.placeholder.com/800x1000'
-            ></img>
-            <div className='project-titles text-center'>Test</div>
-          </div>
-          <div className='col-md-4'>
-            <img
-              className='img-fluid'
-              src='https://via.placeholder.com/800x1000'
-            ></img>
-            <div className='project-titles text-center'>Test</div>
-          </div>
+        {projectsTwo.map((data, index) => (
+            <div className='col-md-4' key={`project-column-${index}`}>
+              <img className='img-fluid' src={data.image} alt={data.alt}></img>
+              <div className='project-titles text-center'>{data.title}</div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
