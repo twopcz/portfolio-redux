aab3786a9ceffaad8ef3e791cf396de1f98a6dba
Justin <twoplatinumchainz@gmail.com>
2020-04-19 11:31:01 -0700

    Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from '../Nav';
+import Content from '../Content';
+import Footer from '../Footer';
+
+function Home() {
+  return (
+    <div className='Home'>
+      <Nav />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
