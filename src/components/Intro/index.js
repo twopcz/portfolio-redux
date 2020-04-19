78a53c81b41371d8e2becba78d151eaa3013ec1f
Justin <twoplatinumchainz@gmail.com>
2020-04-19 15:28:49 -0700

    Extracted inline style to style.css

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
-
-const divStyle = {
-  background: 'red',
-  height: '85vh',
-};
+import './style.css';
 
 function Intro() {
   return (
-    <div className='row' style={divStyle}>
-      <div className='col-md-6 my-auto px-5'>
-        <h1 className='display-4'>
-          <span className='font-weight-bold'>JUSTIN LE</span>
-        </h1>
-        <p className='text-muted'>
+    <div className='row' id='intro'>
+      <div className='my-auto px-5'>
+        <h1 className='display-3'>JUSTIN LE</h1>
+        <p className='lead'>
           a full stack dev who is passionate about style and design
         </p>
       </div>
-      <div className='col-md-6'></div>
     </div>
   );
 }
