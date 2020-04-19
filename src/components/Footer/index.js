2aa3c39e235dfa3f5cc11d1ece1534c52f58da86
Justin <twoplatinumchainz@gmail.com>
2020-04-18 22:04:36 -0700

    Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+function Footer() {
+  return (
+    <footer class='row footer justify-content-center' id='copyright-container'>
+      <div>
+        <span>
+          copyright &copy; 2020 <b>|</b> handcrafted by <b>me</b>
+        </span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
