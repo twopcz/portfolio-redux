71dabfdeeb49929de4f0873a6334b7b2eb309baa
Justin <twoplatinumchainz@gmail.com>
2020-04-19 13:40:16 -0700

    Fixed responsive layout on mobile

--- src/components/NavBrand/index.js
+++ src/components/NavBrand/index.js
@@ -4,13 +4,15 @@ import './style.css';
 function NavBrand() {
   return (
     <div className='col-md-4'>
-      <a
-        className='navbar-brand text-center rounded-circle float-left'
-        href='https://justinle.page'
-        id='logo-name'
-      >
-        JL
-      </a>
+      <div className='text-center text-md-left'>
+        <a
+          className='navbar-brand text-center rounded-circle'
+          href='https://justinle.page'
+          id='logo-name'
+        >
+          JL
+        </a>
+      </div>
     </div>
   );
 }
