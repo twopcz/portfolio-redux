3ddf629f0a9f4e76336b0860b37558669d035f5e
Justin <me@justinle.page>
2020-05-17 19:13:17 -0700

    Updated propTypes

--- src/components/ProjectRow/index.js
+++ src/components/ProjectRow/index.js
@@ -3,35 +3,37 @@ import PropTypes from 'prop-types';
 import { Col, Container, Row } from 'react-bootstrap';
 
 function ProjectRows(props) {
-  return (
-    <Row className='justify-content-center' id={props.rowID}>
-      <Container className='d-flex flex-column flex-md-row'>
-        {props.projects.map((data, index) => (
-          <Col key={`project-column-${index}`}>
-            <a href={data.link}>
-              <div className='project-container'>
-                <img
-                  className='img-fluid'
-                  src={data.image}
-                  alt={data.alt}
-                ></img>
-                <div className='project-titles text-center'>{data.title}</div>
-              </div>
-            </a>
-          </Col>
-        ))}
-      </Container>
-    </Row>
-  );
+    return (
+        <Row className="justify-content-center" id={props.rowID}>
+            <Container className="d-flex flex-column flex-md-row">
+                {props.projects.map((data, index) => (
+                    <Col key={`project-column-${index}`}>
+                        <a href={data.link}>
+                            <div className="project-container">
+                                <img
+                                    className="img-fluid"
+                                    src={data.image}
+                                    alt={data.alt}
+                                ></img>
+                                <div className="project-titles text-center">
+                                    {data.title}
+                                </div>
+                            </div>
+                        </a>
+                    </Col>
+                ))}
+            </Container>
+        </Row>
+    );
 }
 
 export default ProjectRows;
 
 ProjectRows.propTypes = {
-  link: PropTypes.string,
-  rowID: PropTypes.number,
-  projects: PropTypes.array.isRequired,
-  image: PropTypes.string,
-  alt: PropTypes.string,
-  title: PropTypes.string
+    link: PropTypes.string,
+    rowID: PropTypes.string,
+    projects: PropTypes.array.isRequired,
+    image: PropTypes.string,
+    alt: PropTypes.string,
+    title: PropTypes.string
 };
