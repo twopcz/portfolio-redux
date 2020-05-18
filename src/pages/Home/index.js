7c6ebc4bf1e858818aa1a29fe682cdd3bffeffb6
Justin <me@justinle.page>
2020-05-18 01:58:26 -0700

    moved to pages dir

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Content from '../../components/Content';
+
+function Home() {
+    return (
+        <div className="Home">
+            <Content />
+        </div>
+    );
+}
+
+export default Home;
