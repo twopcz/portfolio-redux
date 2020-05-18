b13dff7a641a7a62a9dc1419268bd91f6809cddf
Justin <me@justinle.page>
2020-05-17 22:35:22 -0700

    added pt

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -5,13 +5,13 @@ import Projects from '../Projects';
 import { Container } from 'react-bootstrap';
 
 function Content() {
-  return (
-    <Container fluid>
-      <Intro />
-      <Projects />
-      <About />
-    </Container>
-  );
+    return (
+        <Container fluid className="pt-5 pt-md-0">
+            <Intro />
+            <Projects />
+            <About />
+        </Container>
+    );
 }
 
 export default Content;
