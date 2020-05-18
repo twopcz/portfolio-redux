d0fb6fe7671f4bc5f6be3bc926634a53ee52726a
Justin <me@justinle.page>
2020-05-17 23:01:47 -0700

    add target=_blank

--- src/components/NavLink/index.js
+++ src/components/NavLink/index.js
@@ -3,29 +3,34 @@ import PropTypes from 'prop-types';
 import { Col } from 'react-bootstrap';
 
 function NavLink(props) {
-  return (
-    <Col>
-      <ul className={props.ul}>
-        {props.links.map((data, index) => (
-          <li className={props.li} key={`${index}-li`}>
-            <a href={data.link} title={data.title} target={data.target}>
-              {data.page}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </Col>
-  );
+    return (
+        <Col>
+            <ul className={props.ul}>
+                {props.links.map((data, index) => (
+                    <li className={props.li} key={`${index}-li`}>
+                        <a
+                            href={data.link}
+                            title={data.title}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {data.page}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </Col>
+    );
 }
 
 export default NavLink;
 
 NavLink.propTypes = {
-  alt: PropTypes.string,
-  li: PropTypes.string,
-  link: PropTypes.string,
-  links: PropTypes.array.isRequired,
-  page: PropTypes.string,
-  target: PropTypes.string,
-  ul: PropTypes.string,
+    alt: PropTypes.string,
+    li: PropTypes.string,
+    link: PropTypes.string,
+    links: PropTypes.array.isRequired,
+    page: PropTypes.string,
+    target: PropTypes.string,
+    ul: PropTypes.string
 };
