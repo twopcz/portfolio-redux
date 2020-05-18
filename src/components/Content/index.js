3af6b16cc0d43ff965c83ee2000bc9142fffe3e5
Justin <me@justinle.page>
2020-05-17 18:59:23 -0700

    Revert "minor changes"
    This reverts commit c0262441fc27308f30315df3d577ea14d373384b.

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import Intro from '../Intro';
 import About from '../About';
-import ProjectPreviews from '../ProjectPreviews';
-import Connect from '../Connect';
-import { Container } from 'react-bootstrap';
-
 import Projects from '../Projects';
+import { Container } from 'react-bootstrap';
 
 function Content() {
   return (
     <Container fluid>
       <Intro />
-      {/* <ProjectPreviews /> */}
       <Projects />
       <About />
-      <Connect />
     </Container>
   );
 }
