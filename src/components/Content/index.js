c0262441fc27308f30315df3d577ea14d373384b
Justin <me@justinle.page>
2020-05-17 18:55:24 -0700

    minor changes

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Intro from '../Intro';
 import About from '../About';
-import Projects from '../Projects';
+import ProjectPreviews from '../ProjectPreviews';
+import Connect from '../Connect';
 import { Container } from 'react-bootstrap';
+
+import Projects from '../Projects';
 
 function Content() {
   return (
     <Container fluid>
       <Intro />
+      {/* <ProjectPreviews /> */}
       <Projects />
       <About />
+      <Connect />
     </Container>
   );
 }
