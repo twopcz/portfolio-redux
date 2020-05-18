b36b9a7932fa2af9e23d87849783c9db11af8f93
Justin <me@justinle.page>
2020-05-18 01:57:05 -0700

    added keys

--- src/components/ComponentLink/index.js
+++ src/components/ComponentLink/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
 import { Col } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import React from 'react';
 
 export default function ComponentLink(props) {
     return (
         <Col>
             <ul className={props.ul}>
                 {props.links.map((data, index) => (
-                    <LinkContainer to={data.link}>
+                    <LinkContainer to={data.link} key={`nav-link-${index}`}>
                         <li className={props.li} key={`${index}-li`}>
                             <a href={data.link} title={data.title}>
                                 {data.page}
                             </a>
-                            {/* {data.page} */}
                         </li>
                     </LinkContainer>
                 ))}
