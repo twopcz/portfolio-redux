a98aa0897dcc62048a0bfa1e5e7f9a76585bc393
Justin <me@justinle.page>
2020-05-20 02:46:15 -0700

    changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
