c6c9a93bca7f5c70a03b6c328d6096685387253d
Justin <me@justinle.page>
2020-05-20 02:56:26 -0700

    adjusted routing syntax

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
             <div className="App" id="top">
                 <Switch>
                     <Route exact path="/" component={Home} />
-                    <Route exact path="/projects" component={Projects} />
+                    <Route path="/projects" component={Projects} />
                 </Switch>
                 <Footer />
             </div>
