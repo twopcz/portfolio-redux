d2987ea32424f73d9d3c43861c33fcc4cb00e1bf
Justin <me@justinle.page>
2020-05-19 23:25:47 -0700

    Redid app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import About from './pages/About';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Contact from './pages/Contact';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import Nav from './components/Nav';
+// import Nav from './components/Nav';
 import Projects from './pages/Projects';
 import React from 'react';
 
 function App() {
     return (
         <Router>
-            <div className="App">
-                <Nav />
+            <div className="App" id="top">
                 <Switch>
                     <Route exact path="/">
                         <Home />
                     </Route>
-                    <Route exact path="/about">
-                        <About />
-                    </Route>
                     <Route path="/projects">
                         <Projects />
-                    </Route>
-                    <Route exact path="/contact">
-                        <Contact />
                     </Route>
                 </Switch>
                 <Footer />
