57cd0769af313e03800ffb32721dba44f4caae92
Justin <me@justinle.page>
2020-05-24 13:47:41 -0700

    added technologies

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -9,7 +9,7 @@ function Content() {
     const about = {
         heading: 'about',
         id: 'about',
-        body: `Born and raised in Seattle, WA, I have always had a penchant for building and creating things. Endlessly curious about how things functioned, disassembling and reconstructing everything I could get my hands on was more than just a phase. That later evolved into a hobby centered around computers/technology and becoming a developer was the next logical transition. I really enjoy designing/creating things that look great and function well from the ground up. When I am not on the computer, I enjoy taking photos, playing sports or working on cars.`
+        body: `Born and raised in Seattle, WA, I have always had a penchant for building and creating things. Endlessly curious about how things functioned, disassembling and reconstructing everything I could get my hands on was more than just a phase. That later evolved into a hobby centered around computers/technology and becoming a developer was the next logical transition. I really enjoy designing/creating things that look great and function well from the ground up. When I am not on the computer, I like taking photos, playing sports or working on cars.`
     };
 
     const contact = {
@@ -24,7 +24,27 @@ function Content() {
             </a>
         ),
         id: 'contact',
-        body: `Let's build something together. Get in touch with me via e-mail by clicking 'contact,' or tap in up top to see my GitHub, LinkedIn, and digital resume.`
+        body: `Let's collaborate. Get in touch with me via e-mail by clicking 'contact,' or tap in up top to see my GitHub, LinkedIn, and digital resume.`
+    };
+
+    const technologies = {
+        heading: 'technologies',
+        id: 'technologies',
+        body: (
+            <>
+                <span class="badge badge-light">JavaScript</span>{' '}
+                <span class="badge badge-light">HTML5</span>{' '}
+                <span class="badge badge-light">CSS3</span>{' '}
+                <span class="badge badge-light">MongoDB</span>{' '}
+                <span class="badge badge-light">MySQL</span>{' '}
+                <span class="badge badge-light">Express.js</span>{' '}
+                <span class="badge badge-light">React.js</span>{' '}
+                <span class="badge badge-light">Node.js</span>{' '}
+                <span class="badge badge-light">Bootstrap</span>{' '}
+                <span class="badge badge-light">MySQL</span>{' '}
+                <span class="badge badge-light">Next.js</span>{' '}
+            </>
+        )
     };
 
     return (
@@ -32,6 +52,7 @@ function Content() {
             <Intro />
             <ProjectPreview />
             <Info {...about} />
+            <Info {...technologies} />
             <Info {...contact} />
         </Container>
     );
